e9f6903a8053f217993b6791be7d40ef5e4fb596
alkisax <alkisax@gmail.com>
2025-11-03 14:16:54 +0200

    finished part 10

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,17 +11,30 @@ import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+// export const totalPaidInvoices = async () => {
+//   try {
+
+//     const data = await sql<InvoicesTable[]>`SELECT [.....] `
+
+//     return data
+    
+//   } catch (error) {
+//     console.error('Database Error:', error);
+//     throw new Error('Failed to fetch totalPaidInvoices data.');    
+//   }
+// }
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
